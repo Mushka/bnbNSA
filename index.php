745a30e5c79ecb1cec153b8b715cba64921ad981
Genjutsu <arhyou@gmail.com>
2014-03-19 20:57:55 -0700

    methods test ground

--- index.php
+++ index.php
@@ -1,32 +1,56 @@
-<?php
-
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-  include('function.php');
-
-  
- $rfid =  (isset($_POST['rfid'])) ? $_POST['rfid'] : 1;
- 
-      
 
 
-$bnbnsa = new bnbnsa();
-echo $bnbnsa->checkrfid($rfid); 
-}
+<html ng-app>
+<head>
+	<title>bnbNSA webapp project</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="controller.js"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<head>
+<body>
+	<div id="container" >
+		<form ng-submit='TestController()' ng-keypress="clear()">
+			<input type="text" id="loginField" ng-model='user.login' autofocus="true"  />
+		</form>
+		<form method="POST" action="api.php">
+			<input type="text" name="rfid" autofocus="true"  />
+				<input type="submit" name="submit" value="go"  />
+		</form>
+		<p id="welcomeMessage">{{user.login}}</p>
+	</div>
+	<div id="placeholder"></div>
 
 
 
-?>
+
+
+<script>
+$(document).ready(function(){
+  $("button").click(function(){
+    $.post("api.php",
+    {
+      action:"getuserinfo",
+      rfid:"15507890"
+    },
+    function(data,status){
+     
+});
+    });
+  });
+
+$.getJSON( "http://localhost/club/api.php?action=getuserinfo&rfid=15507890", function( json ) {
+  document.getElementById("welcomeMessage").innerHTML=json.uname+" "+json.urfid;
+});
+</script>
+
+
+<button>zab</button>
 
 
 
-<form method="POST" action="">
-<table>
- <tr><td>post:</td><td><input type="text" name="rfid" /></td></tr>
-
-</table>
-
-<input type="submit" name="login" value="Log In" />
-</form>
-
-
+</body>
+</html>
+</body>
+</html>
